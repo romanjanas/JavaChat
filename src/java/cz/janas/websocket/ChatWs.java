ea05131d8ea02f6434ef4f8da43d3f76faeb2b2d
janas <janas@thinkpad>
2016-08-25 21:53:52 +0200

    Session handler changed to static. Added comments.

--- src/java/cz/janas/websocket/ChatWs.java
+++ src/java/cz/janas/websocket/ChatWs.java
@@ -1,4 +1,6 @@
 package cz.janas.websocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -8,32 +10,51 @@ import javax.websocket.OnError;
 import javax.enterprise.context.ApplicationScoped;
 
 /**
- *
+ * WebSocket server for chatting.
  * @author janas
  */
 @ApplicationScoped
 @ServerEndpoint("/chatws")
 public class ChatWs {
     
-    private final SessionHandler sessionHandler = new SessionHandler();
+    // provides information about connected clients
+    private static final SessionHandler sessionHandler = new SessionHandler();
     
+    /**
+     * Adds client to connected clients list.
+     * @param session connected client
+     */
     @OnOpen
     public void onOpen(Session session) {
         sessionHandler.addSession(session);
     }
     
+    /**
+     * 
+     * @param message
+     * @param session 
+     */
     @OnMessage
     public void onMessage(String message, Session session) {
         sessionHandler.sendToAllClients(message);
     }
     
+    /**
+     * Removes client from connected clients list.
+     * @param session disconnected client
+     */
     @OnClose
     public void onClose(Session session) {
         sessionHandler.removeSession(session);
     }
     
+    /**
+     * Logs communication error.
+     * @param error 
+     */
     @OnError
     public void onError(Throwable error) {
-        
+        Logger.getLogger(ChatWs.class.getName()).log(Level.SEVERE,
+                "Communication error.", error.getMessage());
     }
 }
--- src/java/cz/janas/websocket/SessionHandler.java
+++ src/java/cz/janas/websocket/SessionHandler.java
@@ -10,32 +10,47 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.websocket.Session;
 
 /**
- *
+ * Handler for connected clients.
  * @author janas
  */
 @ApplicationScoped
 public class SessionHandler {
     
+    // List of connected clients
     private final List<Session> sessions = Collections
             .synchronizedList(new LinkedList<Session>());
 
-    public SessionHandler() {
-    }
-    
+    /**
+     * Add client to list of all clients.
+     * @param session client (session) to add
+     */
     public void addSession(Session session) {
         sessions.add(session);
+        Logger.getLogger(ChatWs.class.getName()).log(Level.FINE,
+                "Client was added", session);
     }
     
+    /**
+     * Removes client from clients list.
+     * @param session client to remove
+     */
     public void removeSession(Session session) {
         sessions.remove(session);
+        Logger.getLogger(ChatWs.class.getName()).log(Level.FINE,
+                "Client was removed", session);
     }
 
+    /**
+     * Broadcast message to all clients.
+     * @param message message to broadcast
+     */
     void sendToAllClients(String message) {
         sessions.forEach(s -> {
             try {
                 ((Session) s).getBasicRemote().sendText(message);
             } catch (IOException ex) {
-                Logger.getLogger(ChatWs.class.getName()).log(Level.SEVERE, null,
+                Logger.getLogger(ChatWs.class.getName()).log(Level.SEVERE,
+                        "Sending message to all clients failed.",
                         ex);
             }
         });
